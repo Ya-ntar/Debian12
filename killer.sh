1885c06650c4528daca3eb6ce8a7f024eb2c0f03
ya_ntar <87314636+Ya-ntar@users.noreply.github.com>
2026-03-20 17:32:16 +0300

    Add files via upload

---
+++ killer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+INTERVAL=60   
+ITERATIONS=5  
+
+for ((i=1;i<=ITERATIONS;i++)); do
+    echo "Iteration $i: Checking high CPU processes..."
+    PIDS=$(ps -eo pid,pcpu --no-headers | awk '$2>80 {print $1}')
+    for pid in $PIDS; do
+        echo "Killing process $pid consuming high CPU"
+        kill -9 $pid
+    done
+    
+    sleep $INTERVAL
+done
