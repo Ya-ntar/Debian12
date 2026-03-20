1bc3ceda69dea2dcbfaa867d680db5fbadef8f4c
ya_ntar <87314636+Ya-ntar@users.noreply.github.com>
2026-03-20 17:21:25 +0300

    Format io_stress.sh for consistency

--- io_stress.sh
+++ io_stress.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 FILE="testfile.img"
-BLOCK_SIZE=1M   
-FILE_SIZE=1024  
-
+BLOCK_SIZE=1M 
+FILE_SIZE=1024
 if [ ! -f "$FILE" ]; then
     echo "Creating $FILE (~1GB)..."
     dd if=/dev/zero of="$FILE" bs=$BLOCK_SIZE count=$FILE_SIZE status=none
