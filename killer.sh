6c6ed613a2273962b236aaaf24110b067e05d561
ya_ntar <87314636+Ya-ntar@users.noreply.github.com>
2026-03-20 17:38:10 +0300

    Fix formatting issues in killer.sh script

--- killer.sh
+++ killer.sh
@@ -3,7 +3,7 @@
 INTERVAL=60   
 ITERATIONS=5  
 
-for ((i:=1;i<=ITERATIONS;i++)); do
+for (( i=1; i<=ITERATIONS; i++ )); do
     echo "Iteration $i: Checking high CPU processes..."
     PIDS=$(ps -eo pid,pcpu --no-headers | awk '$2>80 {print $1}')
     for pid in $PIDS; do
